bb40f1ffb115e60d3be14710e82d1c9f4a14c323
Dillon Craw <dillon.craw@gmail.com>
2021-09-21 14:00:04 -0600

    Problem 2

--- lab.js
+++ lab.js
@@ -30,6 +30,15 @@ var brittanisBirthYear = 1986
 */
 
 //CODE HERE
+if (amysAge > brittanisAge) {
+  console.log("Amy is older");
+} 
+else if (brittanisAge > amysAge) {
+  console.log("Brittani is older");
+}
+else {
+  console.log("They are the same age");
+}
 
 
 ////////// PROBLEM 3 //////////
